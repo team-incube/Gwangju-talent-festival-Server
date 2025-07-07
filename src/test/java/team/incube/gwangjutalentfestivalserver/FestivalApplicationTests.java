2faf2bab49b16be4ebdec893f09680c310548a2f
enbraining <me@bricn.net>
2025-07-07 19:05:15 +0900

    :fire: 사용되지 않는 테스트 함수 제거

--- src/test/java/team/incube/gwangjutalentfestivalserver/FestivalApplicationTests.java
+++ src/test/java/team/incube/gwangjutalentfestivalserver/FestivalApplicationTests.java
@@ -1,13 +1,6 @@
 package team.incube.gwangjutalentfestivalserver;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FestivalApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+class FestivalApplicationTests { }
