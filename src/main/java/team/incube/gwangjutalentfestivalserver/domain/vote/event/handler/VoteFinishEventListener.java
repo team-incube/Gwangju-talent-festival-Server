a2519dc36a11297f6007fd9ac1a18bec88da5435
seungchan <s24041@gsm.hs.kr>
2025-08-18 15:46:42 +0900

    :sparkles: VoteFinishEventListener 추가

---
+++ src/main/java/team/incube/gwangjutalentfestivalserver/domain/vote/event/handler/VoteFinishEventListener.java
@@ -0,0 +1,33 @@
+package team.incube.gwangjutalentfestivalserver.domain.vote.event.handler;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+import team.incube.gwangjutalentfestivalserver.domain.vote.event.VoteFinishEvent;
+import team.incube.gwangjutalentfestivalserver.global.sse.VoteSseEmitterManager;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class VoteFinishEventListener {
+
+    private final VoteSseEmitterManager sseEmitterManager;
+
+    @Async("asyncExecutor")
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void onVoteFinish(VoteFinishEvent event) {
+        for (SseEmitter emitter : sseEmitterManager.getAllEmitters()) {
+            try {
+                emitter.send(SseEmitter.event()
+                        .name("VOTE_FINISH")
+                        .data(event));
+            } catch (IOException e) {
+                emitter.completeWithError(e);
+            }
+        }
+    }
+}
