4026c4decf9ac33afd09e8ff06b4cffc99d7f419
seungchan <s24041@gsm.hs.kr>
2025-08-18 15:45:24 +0900

    :sparkles: VoteChangeEvent

---
+++ src/main/java/team/incube/gwangjutalentfestivalserver/domain/vote/event/VoteChangeEvent.java
@@ -0,0 +1,12 @@
+package team.incube.gwangjutalentfestivalserver.domain.vote.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class VoteChangeEvent {
+    private Long teamId;
+    private String teamName;
+    private int currentVoteCount;
+}
