08b1c43a51248d37aa671bda3e77201a5a2b7437
seungchan <s24041@gsm.hs.kr>
2025-08-19 20:03:45 +0900

    :recycle: record에서 기본class로 변경

--- src/main/java/team/incube/gwangjutalentfestivalserver/domain/vote/dto/request/VoteParticipateRequest.java
+++ src/main/java/team/incube/gwangjutalentfestivalserver/domain/vote/dto/request/VoteParticipateRequest.java
@@ -3,8 +3,17 @@ package team.incube.gwangjutalentfestivalserver.domain.vote.dto.request;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-public record VoteParticipateRequest(
-        @NotNull Long teamId,
-        @Min(0) @Max(3) int star
-) {}
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class VoteParticipateRequest {
+    @NotNull
+    private Long teamId;
+
+    @Min(0) @Max(3)
+    private int star;
+}
