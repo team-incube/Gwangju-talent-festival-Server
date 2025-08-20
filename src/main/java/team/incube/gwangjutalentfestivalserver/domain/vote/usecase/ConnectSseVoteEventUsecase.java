0fdb8c931fbe29ae3e0bad8e87e16ca46223516b
seungchan <s24041@gsm.hs.kr>
2025-08-20 17:52:10 +0900

    :recycle: sse 연결하고 이벤트 발행하지 않도록 변경

--- src/main/java/team/incube/gwangjutalentfestivalserver/domain/vote/usecase/ConnectSseVoteEventUsecase.java
+++ src/main/java/team/incube/gwangjutalentfestivalserver/domain/vote/usecase/ConnectSseVoteEventUsecase.java
@@ -5,8 +5,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import team.incube.gwangjutalentfestivalserver.global.sse.VoteSseEmitterManager;
 
-import java.io.IOException;
-
 @Service
 @RequiredArgsConstructor
 public class ConnectSseVoteEventUsecase {
@@ -14,14 +12,6 @@ public class ConnectSseVoteEventUsecase {
     private final VoteSseEmitterManager sseEmitterManager;
 
     public SseEmitter execute(Long teamId) {
-        SseEmitter emitter = sseEmitterManager.addEmitter(teamId);
-
-        try {
-            emitter.send(SseEmitter.event().name("INIT").data("connected"));
-        } catch (IOException e) {
-            emitter.completeWithError(e);
-        }
-
-        return emitter;
+        return sseEmitterManager.addEmitter(teamId);
     }
 }
