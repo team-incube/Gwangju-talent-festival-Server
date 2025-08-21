2c09e87c742813c1be069d8f1a2c5b0e4472ad81
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-08-22 08:13:02 +0900

    :recycle: 클래스 명 수정

--- src/main/java/team/incube/gwangjutalentfestivalserver/global/sse/SeatSseEmitterManager.java
+++ src/main/java/team/incube/gwangjutalentfestivalserver/global/sse/SeatSseEmitterManager.java
@@ -6,33 +6,34 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import java.util.Collection;
 import java.util.Map;
 import java.util.Optional;
+import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Component
-public class SseEmitterManager {
-    private static final long SSE_TIMEOUT_MILLIS = 30 * 60 * 1000L; // 30분
+public class SeatSseEmitterManager {
+    private static final long SSE_TIMEOUT_MILLIS = 60 * 60 * 1000L; // 60분
 
-    private final Map<String, SseEmitter> emitters = new ConcurrentHashMap<>();
+    private final Map<UUID, SseEmitter> emitters = new ConcurrentHashMap<>();
 
-    public SseEmitter addEmitter(String phoneNumber) {
+    public SseEmitter addEmitter(UUID userId) {
         SseEmitter emitter = new SseEmitter(SSE_TIMEOUT_MILLIS);
-        emitters.put(phoneNumber, emitter);
+        emitters.put(userId, emitter);
 
-        emitter.onCompletion(() -> emitters.remove(phoneNumber));
+        emitter.onCompletion(() -> emitters.remove(userId));
         emitter.onTimeout(() -> {
             emitter.complete();
-            emitters.remove(phoneNumber);
+            emitters.remove(userId);
         });
         emitter.onError(e -> {
             emitter.completeWithError(e);
-            emitters.remove(phoneNumber);
+            emitters.remove(userId);
         });
 
         return emitter;
     }
 
-    public Optional<SseEmitter> getEmitter(String phoneNumber) {
-        return Optional.ofNullable(emitters.get(phoneNumber));
+    public Optional<SseEmitter> getEmitter(UUID userId) {
+        return Optional.ofNullable(emitters.get(userId));
     }
 
     public Collection<SseEmitter> getAllEmitters() {
