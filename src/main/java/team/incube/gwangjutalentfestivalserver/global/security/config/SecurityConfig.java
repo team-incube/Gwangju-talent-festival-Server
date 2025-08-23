cb9fe0a78c83feaf492b5ae681137404c8fc35a4
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-08-23 15:49:28 +0900

    :sparkles: cors 설정 추가

--- src/main/java/team/incube/gwangjutalentfestivalserver/global/security/config/SecurityConfig.java
+++ src/main/java/team/incube/gwangjutalentfestivalserver/global/security/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -46,7 +47,7 @@ public class SecurityConfig {
 				.requestMatchers(HttpMethod.POST, "/vote/{teamId}").hasAuthority(Role.ROLE_ADMIN.name())
 				.requestMatchers(HttpMethod.DELETE, "/vote/{teamId}").hasAuthority(Role.ROLE_ADMIN.name())
 			)
-                .cors(cors -> {})
+            .cors(Customizer.withDefaults())
 			.csrf(AbstractHttpConfigurer::disable)
 			.formLogin(AbstractHttpConfigurer::disable)
 			.httpBasic(AbstractHttpConfigurer::disable)
