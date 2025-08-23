f95dc9d893a69746f0d89e5abf6a42d84cdd75f3
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-08-23 12:18:44 +0900

    :fire: SseAuthenticationFilter

--- src/main/java/team/incube/gwangjutalentfestivalserver/global/security/config/SecurityConfig.java
+++ src/main/java/team/incube/gwangjutalentfestivalserver/global/security/config/SecurityConfig.java
@@ -1,18 +1,17 @@
 package team.incube.gwangjutalentfestivalserver.global.security.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import team.incube.gwangjutalentfestivalserver.domain.user.enums.Role;
-import team.incube.gwangjutalentfestivalserver.global.security.filter.JwtFilter;
-import team.incube.gwangjutalentfestivalserver.global.security.filter.SseAuthenticationFilter;
-import team.incube.gwangjutalentfestivalserver.global.security.jwt.JwtProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import team.incube.gwangjutalentfestivalserver.domain.user.enums.Role;
+import team.incube.gwangjutalentfestivalserver.global.security.filter.JwtFilter;
+import team.incube.gwangjutalentfestivalserver.global.security.jwt.JwtProvider;
 
 @Configuration
 @RequiredArgsConstructor
@@ -22,7 +21,6 @@ public class SecurityConfig {
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		JwtFilter jwtFilter = new JwtFilter(jwtProvider);
-		SseAuthenticationFilter sseAuthenticationFilter = new SseAuthenticationFilter(jwtProvider);
 
 		return http
 			.authorizeHttpRequests(it -> it
@@ -55,7 +53,6 @@ public class SecurityConfig {
 			.sessionManagement (it ->
 				it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 			)
-			.addFilterBefore(sseAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
 			.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
 			.build();
 	}
