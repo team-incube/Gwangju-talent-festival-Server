198eac6a1995fe4563edf175b1e295551a2fb4ee
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-08-23 17:18:33 +0900

    :recycle: 팀 관련 api 매핑

--- src/main/java/team/incube/gwangjutalentfestivalserver/domain/team/controller/TeamController.java
+++ src/main/java/team/incube/gwangjutalentfestivalserver/domain/team/controller/TeamController.java
@@ -1,12 +1,16 @@
 package team.incube.gwangjutalentfestivalserver.domain.team.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import team.incube.gwangjutalentfestivalserver.domain.team.dto.request.CreateTeamRequest;
 import team.incube.gwangjutalentfestivalserver.domain.team.dto.response.GetAllTeamsResponse;
+import team.incube.gwangjutalentfestivalserver.domain.team.dto.response.GetTeamRankingResponse;
+import team.incube.gwangjutalentfestivalserver.domain.team.usecase.ChangePerformanceTeamUsecase;
+import team.incube.gwangjutalentfestivalserver.domain.team.usecase.CreateTeamUsecase;
 import team.incube.gwangjutalentfestivalserver.domain.team.usecase.FindAllTeamsUsecase;
+import team.incube.gwangjutalentfestivalserver.domain.team.usecase.FindTeamRankingUsecase;
 
 import java.util.List;
 
@@ -15,10 +19,31 @@ import java.util.List;
 @RequiredArgsConstructor
 public class TeamController {
     private final FindAllTeamsUsecase findAllTeamsUsecase;
+    private final ChangePerformanceTeamUsecase changePerformanceTeamUsecase;
+    private final CreateTeamUsecase createTeamUsecase;
+    private final FindTeamRankingUsecase findTeamRankingUsecase;
 
     @GetMapping
     public ResponseEntity<List<GetAllTeamsResponse>> getAllTeams() {
         List<GetAllTeamsResponse> responses = findAllTeamsUsecase.execute();
+        return ResponseEntity.ok(responses);
+    }
+
+    @PatchMapping("/{team_id}")
+    public ResponseEntity<Void> changePerformanceTeam(@PathVariable("team_id") Long teamId) {
+        changePerformanceTeamUsecase.execute(teamId);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createTeam(@RequestBody @Valid CreateTeamRequest request) {
+        createTeamUsecase.execute(request.getTeamName());
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/ranking")
+    public ResponseEntity<List<GetTeamRankingResponse>> getTeamRanking() {
+        List<GetTeamRankingResponse> responses = findTeamRankingUsecase.execute();
         return ResponseEntity.ok(responses);
     }
 }
