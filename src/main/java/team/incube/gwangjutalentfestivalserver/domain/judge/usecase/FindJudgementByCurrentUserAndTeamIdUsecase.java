5d53118c6d6218121d8c6a2903c77ec5d2c0a5bc
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-09-09 12:24:31 +0900

    :recycle: null 체크 로직 Optional로 수정

--- src/main/java/team/incube/gwangjutalentfestivalserver/domain/judge/usecase/FindJudgementByCurrentUserAndTeamIdUsecase.java
+++ src/main/java/team/incube/gwangjutalentfestivalserver/domain/judge/usecase/FindJudgementByCurrentUserAndTeamIdUsecase.java
@@ -32,9 +32,10 @@ public class FindJudgementByCurrentUserAndTeamIdUsecase {
 
         Judgement jm = judgementRepository.findByUserAndTeam(user, team).orElse(null);
 
-        int completionExpression   = jm != null ? jm.getCompletionExpression() : 0;
-        int creativityComposition  = jm != null ? jm.getCreativityComposition() : 0;
-        int stageMannerPerformance = jm != null ? jm.getStageMannerPerformance() : 0;
+        int completionExpression   = Optional.ofNullable(jm).map(Judgement::getCompletionExpression).orElse(0);
+        int creativityComposition  = Optional.ofNullable(jm).map(Judgement::getCreativityComposition).orElse(0);
+        int stageMannerPerformance = Optional.ofNullable(jm).map(Judgement::getStageMannerPerformance).orElse(0);
+
         boolean judged             = jm != null;
         boolean performed          = team.getPerformStatus() != TeamStatus.PENDING;
         int totalScore             = Optional.ofNullable(team.getTotalScore()).orElse(0);
