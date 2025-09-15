cb683052aad2161c1365cdc55b8c0120d4c6fd98
seungchan <s24041@gsm.hs.kr>
2025-09-15 20:05:17 +0900

    :sparkles: RandomReservationExtractor 추가

---
+++ src/main/java/team/incube/gwangjutalentfestivalserver/domain/vote/usecase/RandomReservationExtractor.java
@@ -0,0 +1,32 @@
+package team.incube.gwangjutalentfestivalserver.domain.vote.usecase;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import team.incube.gwangjutalentfestivalserver.domain.seat.entity.SeatReservation;
+import team.incube.gwangjutalentfestivalserver.domain.seat.repository.SeatReservationRepository;
+import team.incube.gwangjutalentfestivalserver.global.exception.HttpException;
+
+import java.util.Collections;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class RandomReservationExtractor {
+
+    private final SeatReservationRepository seatReservationRepository;
+
+    public List<SeatReservation> extractRandomReservations(Long teamId, int count) {
+        List<SeatReservation> reservations = seatReservationRepository.findAllByTeamId(teamId);
+
+        if (reservations.isEmpty()) {
+            throw new HttpException(HttpStatus.BAD_REQUEST, "해당 팀에는 좌석 예약자가 없습니다.");
+        }
+
+        Collections.shuffle(reservations);
+        return reservations.stream()
+                .limit(count)
+                .toList();
+    }
+}
+
