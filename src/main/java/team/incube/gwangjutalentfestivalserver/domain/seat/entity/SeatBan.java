31235f4cd6c6e98bc86743d566de27f85b39dccb
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-09-18 17:05:02 +0900

    :recycle: role 필드 추가

--- src/main/java/team/incube/gwangjutalentfestivalserver/domain/seat/entity/SeatBan.java
+++ src/main/java/team/incube/gwangjutalentfestivalserver/domain/seat/entity/SeatBan.java
@@ -9,6 +9,7 @@ import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 import team.incube.gwangjutalentfestivalserver.domain.seat.entity.embeddable.SeatBanId;
 import team.incube.gwangjutalentfestivalserver.domain.user.entity.User;
+import team.incube.gwangjutalentfestivalserver.domain.user.enums.Role;
 
 @Table(name = "seat_ban")
 @Entity
@@ -18,4 +19,8 @@ import team.incube.gwangjutalentfestivalserver.domain.user.entity.User;
 public class SeatBan {
     @EmbeddedId
     private SeatBanId id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private Role role;
 }
