c3e9d3c84c126cab96d4b9efca922521f1cb265f
seungchan <s24041@gsm.hs.kr>
2026-02-02 16:46:52 +0900

    :sparkles: 필요 메서드 추가

--- src/main/java/team/incube/gwangjutalentfestivalserver/domain/vote/repository/VoteUserRepository.java
+++ src/main/java/team/incube/gwangjutalentfestivalserver/domain/vote/repository/VoteUserRepository.java
@@ -3,8 +3,11 @@ package team.incube.gwangjutalentfestivalserver.domain.vote.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import team.incube.gwangjutalentfestivalserver.domain.vote.entity.VoteUser;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface VoteUserRepository extends JpaRepository<VoteUser, Long> {
     boolean existsByTeamIdAndUserId(Long teamId, UUID userId);
+
+    List<VoteUser> findAllByTeamId(Long teamId);
 }
